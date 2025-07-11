b9cb1d12c2ebefc3cf29d77b9665e0c10f911703
mkirsh123 <142982085+mkirsh123@users.noreply.github.com>
2025-07-11 11:18:36 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -49,7 +49,7 @@ const Login = () => {
       if (success) {
         handleSuccess(message);
         setTimeout(() => {
-          window.location.href = "https://686f813792ae164ed7faffc0--tradingplatformdashboard.netlify.app"; // dashboard project
+          window.location.href = "tradingplatformdashboard.netlify.app"; // dashboard project
         }, 1000);
       } else {
         handleError(message);
